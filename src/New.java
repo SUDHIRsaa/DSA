f65788f20abdcf2a0bb1e6e02b02cc40f5a981a2
SUDHIRsaa <140333096+SUDHIRsaa@users.noreply.github.com>
2024-08-30 20:46:51 +0530

    new

--- src/New.java
+++ src/New.java
@@ -1,6 +1,6 @@
 public class New {
     public static void  main(String[] args){
-        calc(10);
+        calc(9);
 
     }
     public static void calc(int n){
@@ -57,6 +57,169 @@ public class New {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+       for (int i=0;i<=n;i++){
+
+        for(int j=1;j<=2*i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+       }
+       
+       System.out.println();
+       System.out.println();
+       System.out.println();
+       for (int i=n;i>=0;i--){
+
+        for(int j=1;j<=2*i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+       }
+       System.out.println();
+       System.out.println();
+       System.out.println();
+        
+    for (int i= 1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            System.out.print((char)(96+j));
+        }
+        System.out.println();
     }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    for (int i= 1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            System.out.print((char)(96+i));
+        }
+        System.out.println();
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    for (int i= n;i>=0;i--){
+        for (int j=1;j<=i;j++){
+            System.out.print((char)(96+i));
+        }
+        System.out.println();
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    
+
+    // for (char c='a';c<='z';c++){
+    //     for (char j='a';j<=c;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+    for (int i=1; i<=n;i++){
+        for (int k=1;k<=n-i;k++){
+            System.out.print(" ");
+        }
+        for (int j=1;j<=i;j++){
+            System.out.print(j);
+        }
+        
+        System.out.println();
+        
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+
+    for (int i=1; i<=n;i++){
+        for (int k=1;k<=n-i;k++){
+            System.out.print(" ");
+        }
+        for (int j=i;j>=1;j--){
+            System.out.print(j);
+        }
+        
+        System.out.println();
+        
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+
+    for (int i=1; i<=n;i++){
+        for (int k=1;k<=n-i;k++){
+            System.out.print(" ");
+            
+        }
+        for (int j=1;j<=i;j++){
+            System.out.print(j+" ");
+            
+        }
+       
+    System.out.println();
+        
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+
+    for (int i=n; i>=1;i--){
+        for (int k=1;k<=n-i;k++){
+            System.out.print(" ");
+            
+        }
+        for (int j=1;j<=i;j++){
+            System.out.print(j+" ");
+            
+        }
+       
+    System.out.println();
+        
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    for (int i=n; i>=1;i--){
+        for (int k=1;k<=n-i;k++){
+            System.out.print(" ");
+            
+        }
+        for (int j=1;j<=2*i;j++){
+            System.out.print(j+" ");
+            
+        }
+       
+    System.out.println();
+        
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    
+    for (int i=n; i>=1;i--){
+        for (int k=1;k<=n-i;k++){
+            System.out.print(" ");
+            
+        }
+        for (int j=1;j<=2*i-1;j++){
+            System.out.print(j+" ");
+            
+        }
+       
+    System.out.println();
+        
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    
+    
+    }
+
+
+    
+   
    
 }
