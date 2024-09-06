7557bd8b7bb4e1aea110cb90836042f72695d2b2
SUDHIRsaa <140333096+SUDHIRsaa@users.noreply.github.com>
2024-09-06 20:35:47 +0530

    wow

--- src/SS.java
+++ src/SS.java
@@ -1,15 +1,45 @@
 public class SS {
     public static void main(String[] args) {
-        String s="sudhir";
-        System.err.println(s.length());
-        for (int i=0;i<s.length();i++){
-            for(int j=i+1;j<=s.length();j++){
-                String a= s.substring(i, j);                          
-                System.out.println(a);
-            }
-           
+        String s="Samiksha Sudhir Maurya";
+        
+        char ss[]=s.toCharArray();
+        int number=0;
+        for (char c : ss) {
+            number++;
+            // System.out.print(c);
+            
+            
+            
+        }
+        char wow=' ';
+        // System.out.println(ss.length);
+        for(int i=ss.length-1;i>=0;i--){
+
+          wow =ss[i];
+          String wo =  " "+wow;
+          System.out.print(wo);
+          
+
 
         }
+        
+
+
+        
+
+        // System.out.println(number);
+        // System.err.println(s.length());
+        // int count=0;
+        // for (int i=0;i<number;i++){
+        //     for(int j=i+1;j<=number;j++){
+        //         String a= s.substring(i, j);
+        //         count++;                          
+        //         System.out.println(a);
+        //     }
+          
+
+        // }
+        // System.out.println(count);
     }
     
 }
